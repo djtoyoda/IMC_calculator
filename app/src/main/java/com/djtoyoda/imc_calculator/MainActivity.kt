71db3598d68dff657dae028c68274dae7a14c70d
djtoyoda <djtoyoda@gmail.com>
2021-03-30 19:38:42 +0200

    Implemented coloring according to result

--- app/src/main/java/com/djtoyoda/imc_calculator/MainActivity.kt
+++ app/src/main/java/com/djtoyoda/imc_calculator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.djtoyoda.imc_calculator
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -12,14 +16,52 @@ class MainActivity : AppCompatActivity() {
         setListeners()
     }
 
-    private fun setListeners() {
+    fun setListeners() {
         btCalcular.setOnClickListener {
             calcularIMC(etPeso.text.toString(), etAltura.text.toString())
         }
+
+        etAltura.doOnTextChanged {text, start, count, after ->
+            tvResultado.text = ""
+        }
+
+        etPeso.doOnTextChanged {text, start, count, after ->
+            tvResultado.text = ""
+        }
     }
 
-    private fun calcularIMC(peso: String, altura: String) {
-        val imc = peso.toDouble() / altura.toDouble() / altura.toDouble()
-        tvResultado.text = "Seu IMC é $imc"
+    fun calcularIMC(peso: String, altura: String) {
+        if (peso.isNullOrEmpty() || altura.isNullOrEmpty()) {
+            tvResultado.text = "Favor inserir peso e altura"
+        }
+        else {
+            val imc = peso.toDouble() / altura.toDouble() / altura.toDouble()
+            val roundIMC = String.format("%.1f", imc)
+            var doubleIMC = roundIMC.toDouble()
+            tvResultado.text = "Seu IMC é ${String.format("%.1f", imc)}"
+
+            //reset color
+            for (i in 1..7) {
+                val nomeStringFaixa = "Faixa" + i
+                val nomeStringSign = "Sign" + i
+                val id1 = resources.getIdentifier(nomeStringFaixa, "id", packageName)
+                val id2 = resources.getIdentifier(nomeStringSign, "id", packageName)
+                val textReset1 = findViewById<View>(id1) as TextView
+                val textReset2 = findViewById<View>(id2) as TextView
+                textReset1.setTextColor(Color.parseColor("#000000"))
+                textReset2.setTextColor(Color.parseColor("#000000"))
+            }
+
+            //colore faixa do usuario
+            when (doubleIMC){
+                in 0.0 .. 16.9 -> {Faixa1.setTextColor(Color.parseColor("#FF5200")); Sign1.setTextColor(Color.parseColor("#FF5200"))}
+                in 17.0 .. 18.4 -> {Faixa2.setTextColor(Color.parseColor("#FFA300")); Sign2.setTextColor(Color.parseColor("#FFA300"))}
+                in 18.5 .. 24.9 -> {Faixa3.setTextColor(Color.parseColor("#45FF00")); Sign3.setTextColor(Color.parseColor("#45FF00"))}
+                in 25.0 .. 29.9 -> {Faixa4.setTextColor(Color.parseColor("#FFA300")); Sign4.setTextColor(Color.parseColor("#FFA300"))}
+                in 30.0 .. 34.9 -> {Faixa5.setTextColor(Color.parseColor("#FF000D")); Sign5.setTextColor(Color.parseColor("#FF000D"))}
+                in 35.0 .. 39.9 -> {Faixa6.setTextColor(Color.parseColor("#FF000D")); Sign6.setTextColor(Color.parseColor("#FF000D"))}
+                else -> {Faixa7.setTextColor(Color.parseColor("#FF000D")); Sign7.setTextColor(Color.parseColor("#FF000D"))}
+            }
+        }
     }
 }
