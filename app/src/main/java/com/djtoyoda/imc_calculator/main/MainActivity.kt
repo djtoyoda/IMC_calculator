c886d71373aca67964215c85b1dea564d02d4beb
djtoyoda <diogo.toyoda@gmail.com>
2021-04-02 00:20:13 +0200

    Implemented avoid "error" values in the DB

--- app/src/main/java/com/djtoyoda/imc_calculator/main/MainActivity.kt
+++ app/src/main/java/com/djtoyoda/imc_calculator/main/MainActivity.kt
@@ -46,9 +46,12 @@ class MainActivity : AppCompatActivity() {
             val date = LocalDate.now()
             val parsedDate = date.format(DateTimeFormatter.ISO_DATE).toString()
             val imcStr = returnIMC(etPeso.text.toString(), etAltura.text.toString())
-            val db = HelperDB(this)
-            val newIMCData = DataIMC(parsedDate, etPeso.text.toString(), imcStr)
-            db.adicionarIMC(newIMCData)
+
+            if (imcStr != "erro") {
+                val db = HelperDB(this)
+                val newIMCData = DataIMC(parsedDate, etPeso.text.toString(), imcStr)
+                db.adicionarIMC(newIMCData)
+            }
         }
 
         //botao para mostrar historico na activity_historico
@@ -82,51 +85,46 @@ class MainActivity : AppCompatActivity() {
             val imc = peso.toDouble() / altura.toDouble() / altura.toDouble()
             return String.format("%.1f", imc)
         }
-        val x = "erro"
-        return x
+        return "erro"
     }
 
     fun mostrarIMC(imc: String) {
-        if (imc == "erro") {
-            tvResultado.text = "Favor inserir peso e altura"
+        val doubleIMC = imc.toDouble()
+        tvResultado.text = "Seu IMC é $doubleIMC"
+
+        //reset color
+        for (i in 1..7) {
+            val nomeStringFaixa = "Faixa" + i
+            val nomeStringSign = "Sign" + i
+            val id1 = resources.getIdentifier(nomeStringFaixa, "id", packageName)
+            val id2 = resources.getIdentifier(nomeStringSign, "id", packageName)
+            val textReset1 = findViewById<View>(id1) as TextView
+            val textReset2 = findViewById<View>(id2) as TextView
+            textReset1.setTextColor(Color.parseColor("#000000"))
+            textReset2.setTextColor(Color.parseColor("#000000"))
         }
-        else {
-            val doubleIMC = imc.toDouble()
-            tvResultado.text = "Seu IMC é $doubleIMC"
 
-            //reset color
-            for (i in 1..7) {
-                val nomeStringFaixa = "Faixa" + i
-                val nomeStringSign = "Sign" + i
-                val id1 = resources.getIdentifier(nomeStringFaixa, "id", packageName)
-                val id2 = resources.getIdentifier(nomeStringSign, "id", packageName)
-                val textReset1 = findViewById<View>(id1) as TextView
-                val textReset2 = findViewById<View>(id2) as TextView
-                textReset1.setTextColor(Color.parseColor("#000000"))
-                textReset2.setTextColor(Color.parseColor("#000000"))
+        //colore faixa do usuario
+        when (doubleIMC){
+            in 0.0..16.9 -> {
+                Faixa1.setTextColor(Color.parseColor("#FF5200")); Sign1.setTextColor(Color.parseColor("#FF5200"))
             }
-
-            //colore faixa do usuario
-            when (doubleIMC){
-                in 0.0..16.9 -> {
-                    Faixa1.setTextColor(Color.parseColor("#FF5200")); Sign1.setTextColor(Color.parseColor("#FF5200"))
-                }
-                in 17.0..18.4 -> {
-                    Faixa2.setTextColor(Color.parseColor("#FFA300")); Sign2.setTextColor(Color.parseColor("#FFA300"))
-                }
-                in 18.5..24.9 -> {
-                    Faixa3.setTextColor(Color.parseColor("#45FF00")); Sign3.setTextColor(Color.parseColor("#45FF00"))
-                }
-                in 25.0..29.9 -> {
-                    Faixa4.setTextColor(Color.parseColor("#FFA300")); Sign4.setTextColor(Color.parseColor("#FFA300"))
-                }
-                in 30.0..34.9 -> {
-                    Faixa5.setTextColor(Color.parseColor("#FF000D")); Sign5.setTextColor(Color.parseColor("#FF000D"))
-                }
-                in 35.0..39.9 -> {
-                    Faixa6.setTextColor(Color.parseColor("#FF000D")); Sign6.setTextColor(Color.parseColor("#FF000D"))
-                }
-                else -> {Faixa7.setTextColor(Color.parseColor("#FF000D")); Sign7.setTextColor(Color.parseColor("#FF000D"))}
+            in 17.0..18.4 -> {
+                Faixa2.setTextColor(Color.parseColor("#FFA300")); Sign2.setTextColor(Color.parseColor("#FFA300"))
+            }
+            in 18.5..24.9 -> {
+                Faixa3.setTextColor(Color.parseColor("#45FF00")); Sign3.setTextColor(Color.parseColor("#45FF00"))
+            }
+            in 25.0..29.9 -> {
+                Faixa4.setTextColor(Color.parseColor("#FFA300")); Sign4.setTextColor(Color.parseColor("#FFA300"))
+            }
+            in 30.0..34.9 -> {
+                Faixa5.setTextColor(Color.parseColor("#FF000D")); Sign5.setTextColor(Color.parseColor("#FF000D"))
+            }
+            in 35.0..39.9 -> {
+                Faixa6.setTextColor(Color.parseColor("#FF000D")); Sign6.setTextColor(Color.parseColor("#FF000D"))
+            }
+            else -> {Faixa7.setTextColor(Color.parseColor("#FF000D")); Sign7.setTextColor(Color.parseColor("#FF000D"))}
             }
         }
     }
